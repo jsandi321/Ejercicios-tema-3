af76b54b1ac729cad32ed747ff430db924795c44
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2022-05-09 18:33:18 -0600

    Correcion Clase Choche
    Se corrige error en la clase coche y su metodo AgregarPuertas () ahora recibe un parametro que actualiza los atributos de la clase.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,36 @@
 public class Main
 {
     public static void main(String[] args) {
-        //Parte 1
         /*
+        Parte 1
         Crear una funcion con tres parametros que sean numeros que se
         suman entre si y llamarla desde el main
          */
+        int numero1 = 9;
+        int numero2 = 20;
+        int numero3 = 10;
+
+        int resultadoSuma = Sumar(numero1, numero2, numero3);
+        System.out.println(resultadoSuma);
+
+        /*
+            Parte 2
+            Crear una clase coche.
+            Dentro de la clase coche, una variable numérica que almacene
+            el número de puertas que tiene.
+            Una función que incremente el número de puertas que tiene
+            el coche.
+            Crear un objeto miCoche en el main y añadirle una puerta.
+            Mostrar el número de puertas que tiene el objeto.
+         */
         Coche miCoche = new Coche();
-        miCoche.AgregarPuertas();
-        miCoche.AgregarPuertas();
+        int puertas = 2;
+        miCoche.AgregarPuertas(puertas);
         System.out.println(miCoche.puertas);
 
 
     }
-    public int Sumar(int numA, int numB, int numC)
+    public static int Sumar(int numA, int numB, int numC)
     {
         int resultado;
         resultado = numA + numB + numC;
@@ -26,8 +43,8 @@ class Coche
     //Atributos de la clase
     public int puertas = 0;
 
-    public void AgregarPuertas()
+    public void AgregarPuertas(int puertasPa)
     {
-        this.puertas++;
+        this.puertas += puertasPa;
     }
 }
